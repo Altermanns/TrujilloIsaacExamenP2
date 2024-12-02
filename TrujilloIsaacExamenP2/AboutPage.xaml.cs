3b823a2226f2eb8f0e6c55d1c156112150376a68
Altermanns <isaac.277.trujillo@gmail.com>
2024-12-02 08:02:29 -0500

    Contenido de AboutPage

--- TrujilloIsaacExamenP2/AboutPage.xaml.cs
+++ TrujilloIsaacExamenP2/AboutPage.xaml.cs
@@ -6,4 +6,20 @@ public partial class AboutPage : ContentPage
 	{
 		InitializeComponent();
 	}
+    private async void InfoButton_Clicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("Información de mí", "Mi nombre es [Isaac Trujillo]", "Cerrar");
+    }
+
+    private void GitHubButton_Clicked(object sender, EventArgs e)
+    {
+        var githubUrl = "https://github.com/Altermanns/TrujilloIsaacExamenP2.git";
+        Launcher.Default.OpenAsync(githubUrl);
+    }
+
+    private void FavoriteSongButton_Clicked(object sender, EventArgs e)
+    {
+        var songUrl = "https://www.youtube.com/watch?v=Jy1D6caG8nU";
+        Launcher.Default.OpenAsync(songUrl);
+    }
 }
